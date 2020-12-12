35b534a2ef1c8c463912fa95069795f22f0de731
ElinaPark <elinapark0818@gmail.com>
2020-12-12 18:02:30 +0900

    자바 연습

---
+++ study/practice.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperationEx5 {
+    public static void main(String[ ]args) {
+        int num = 0B00000101;
+        System.out.println(num << 2);
+//        System.out.println(num >> 2);
+//        System.out.println(num >>> 2);
+//
+//        num = num << 2;
+//        System.out.println(num);
+//
+    }
+}
