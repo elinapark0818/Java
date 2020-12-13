f999b655de1a6a31f46c36100914e59c3d995312
ElinaPark <elinapark0818@gmail.com>
2020-12-13 23:15:19 +0900

    Do it 자바 연습문제

--- Java/com/yunjung/java/practiceTest.java
+++ Java/com/yunjung/java/practiceTest.java
@@ -14,5 +14,33 @@ public class practiceTest {
         char ch;
         ch = (myAge > teacherAge) ? 'T': 'F';
         System.out.println(ch);
+
+        System.out.println("----------------");
+
+//        int num;
+//        num = -5 + 3 * 10 / 2;
+//        System.out.println(num);
+
+//        int num = 10;
+//        System.out.println(num);
+//        System.out.println(num++);
+//        System.out.println(num);
+//        System.out.println(--num);
+
+        int num1 = 10;
+        int num2 = 20;
+        boolean result;
+
+        result = ((num1 > 10) && (num2 > 10));
+        System.out.println(result);
+        result = ((num1 > 10) || (num2 > 10));
+        System.out.println(result);
+        System.out.println(!result);
+
+
+
+
+
+
     }
 }
