2b3019ec53902df83103c124feaca92a99a42533
elinapark0818@gmail.com <elinapark0818@gmail.com>
2020-12-14 20:48:10 +0900

    p123 연습문제 + 후민

--- Java/com/yunjung/java/test.java
+++ Java/com/yunjung/java/test.java
@@ -23,10 +23,7 @@ public class test {
         }
         System.out.println(num1 + operator + num2 + "=" + result + "입니다.");
 
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        System.out.println("--------------------");
+        printDash();
 
         // 2. 구구단을 짝수 단만 출력하도록 프로그램을 만들어 보세요.
         for (int i = 2; i < 10; i += 2) {   // 2를 더해줌으로써 짝수단 완성
@@ -36,17 +33,14 @@ public class test {
             System.out.println();
         }
 
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        System.out.println("--------------------");
+        printDash();
 
         for (int i = 2; i < 10; i++) {  // 1을 더해줌으로써 그냥 구구단
             if((i % 2) != 0) {  // 짝수가 아니게 if 문을 사용했다
                 for (int j = 1; j < 10; j++) {
                     System.out.println(i + "X" + j + "=" + (i * j));
                 }
-            System.out.println();
+                System.out.println();
             }
         }
         // 2-1 구구단을 짝수단만 출력하도록 continue 를 사용한 프로그램을 만들어보세요.
@@ -57,12 +51,9 @@ public class test {
                     continue;
                 System.out.println(i + "X" + j + "=" + (i * j));
             }
-        System.out.println();
+            System.out.println();
         }
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        System.out.println("--------------------");
+        printDash();
 
         // 3. 구구단을 단보다 곱하는 수가 작거나 같은 경우까지만 출력하는 프로그램을 만들어 보세요.
         int dan;
@@ -75,36 +66,23 @@ public class test {
             System.out.println();
         }
 
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        System.out.println("--------------------");
+        printDash();
 
         // 4. 반복문을 사용하여 *트리를 출력하는 프로그램을 만들어보자.
 
-        int c = 1;
         int d = 0;
         for (int j = 1; j < 8; j++) {
-            for (int m = 10; m > c; m--) {
-                System.out.printf(" ");
+            for (int m = 10; m > j; m--) {
+                System.out.print(" ");
             }
             for (int i = 0; i <= d; i++) {
-                System.out.printf("*");
+                System.out.print("*");
             }
-            System.out.printf("\n");
-            if (j < 10) {
-                c++;
-                d += 2;
-            } else {
-                c --;
-                d -= 2;
-            }
+            System.out.print("\n");
+            d += 2;
         }
 
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        System.out.println("--------------------");
+        printDash();
 
         // 5. 반복문과 조건문을 사용하여 *마름모를 출력하는 프로그램을 만들어보자.
 
@@ -128,7 +106,24 @@ public class test {
         }
 
 
+        printDash();
+        printPyramid();
 
+    }
 
+    private static void printDash() {
+        System.out.println("--------------------");
+        System.out.println("--------------------");
+        System.out.println("--------------------");
+        System.out.println("--------------------");
+    }
+
+    private static void printPyramid() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 8; j > 0 ; j--) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
